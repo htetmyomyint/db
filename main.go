ba5d1928265ecf5cc77be0abcdca9dc687770e6e
htetmyomyint <53549381+htetmyomyint@users.noreply.github.com>
2019-08-20 17:41:15 +0630

    Add files via upload

--- main.go
+++ main.go
@@ -1,11 +1,86 @@
 package main
 
 import (
-	_ "hw/routers"
-	"github.com/astaxie/beego"
+	"crypto/tls"
+	"fmt"
+	"log"
+	"net"
+	"net/mail"
+	"net/smtp"
 )
 
 func main() {
-	beego.Run()
-}
+	from := mail.Address{Name: "", Address: "htetmyomyint.irrahub@gmail.com"}
+	to := mail.Address{Name: "", Address: "htetmyomyint@gmail.com"}
+	subj := "This is the email subject"
+	body := "This is an example body.\n With two lines."
 
+	// Setup headers
+	headers := make(map[string]string)
+	headers["From"] = from.String()
+	headers["To"] = to.String()
+	headers["Subject"] = subj
+
+	// Setup message
+	message := ""
+	for k, v := range headers {
+		message += fmt.Sprintf("%s: %s\r\n", k, v)
+	}
+	message += "\r\n" + body
+
+	// Connect to the SMTP Server
+	servername := "smtp.gmail.com:587"
+
+	host, _, _ := net.SplitHostPort(servername)
+	auth := smtp.PlainAuth("", "htetmyomyint.irrahub@gmail.com", "HTETirrahub#1", host)
+
+	// auth := smtp.PlainAuth("","username@example.tld", "password", host)
+
+	// TLS config
+	tlsconfig := &tls.Config{
+		InsecureSkipVerify: true,
+		ServerName:         host,
+	}
+
+	c, err := smtp.Dial(servername)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	c.StartTLS(tlsconfig)
+
+	// Auth
+	if err = c.Auth(auth); err != nil {
+		log.Panic(err)
+	}
+
+	// To && From
+	if err = c.Mail(from.Address); err != nil {
+		log.Panic(err)
+	}
+
+	if err = c.Rcpt(to.Address); err != nil {
+		log.Panic(err)
+	}
+
+	// Data
+	w, err := c.Data()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	_, err = w.Write([]byte(message))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	err = w.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	c.Quit()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
